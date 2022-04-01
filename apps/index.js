72093b8135c9ee521f9d81697d8907da53a3a2c3
sayed94h <skazimi951@yahoo.com>
2022-04-01 08:38:57 +0200

    v5

--- apps/index.js
+++ apps/index.js
@@ -3,12 +3,6 @@
 const ageEl = document.getElementById("age");
 ageEl ? ageEl.innerHTML = (new Date()).getFullYear() - 1994 : "";
 
-// let url_ = "https://play.google.com/store/apps/details?id=com.aqyanoos.httpresponsecode";
-
-// fetch(url_, { mode: "no-cors" }).then(re => {
-//     console.log("res: ", re);
-// });
-
 const today = Date.now();
 
 const difFRhrc = Math.round((new Date(today) - (new Date("2022-02-09"))) / 1000 / 60 / 60 / 24);
